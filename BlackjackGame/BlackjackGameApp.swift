5a104edd593a90ecfb1de428125c297c2080647e
Jonni Åkesson <devbyjonni@gmail.com>
2025-01-19 13:23:36 +0100

    Initial Commit

---
+++ BlackjackGame/BlackjackGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackjackGameApp.swift
+//  BlackjackGame
+//
+//  Created by Jonni Akesson on 2025-01-19.
+//
+
+import SwiftUI
+
+@main
+struct BlackjackGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
